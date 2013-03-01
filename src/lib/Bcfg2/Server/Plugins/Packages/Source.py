18c024d69b373af53398157c658bcd2265d5d9c0
Alexander Sulfrian <alexander@sulfrian.net>
2013-03-01 01:07:05 +0100

    Packages: Add 'pin' attribute

--- src/lib/Bcfg2/Server/Plugins/Packages/Source.py
+++ src/lib/Bcfg2/Server/Plugins/Packages/Source.py
@@ -198,6 +198,9 @@ class Source(Debuggable):  # pylint: disable=R0902
         #: The "priority" attribute from :attr:`xsource`
         self.priority = xsource.get('priority', 500)
 
+        #: The "pin" attribute from :attr:`xsource`
+        self.pin = xsource.get('pin', '')
+
         #: A list of predicates that are used to determine if this
         #: source applies to a given
         #: :class:`Bcfg2.Server.Plugins.Metadata.ClientMetadata`
@@ -261,12 +264,12 @@ class Source(Debuggable):  # pylint: disable=R0902
             if self.url:
                 usettings = [dict(version=self.version, component=comp,
                                   arch=arch, debsrc=self.debsrc,
-                                  priority=self.priority)
+                                  priority=self.priority, pin=self.pin)
                              for comp in self.components]
             else:  # rawurl given
                 usettings = [dict(version=self.version, component=None,
                                   arch=arch, debsrc=self.debsrc,
-                                  priority=self.priority)]
+                                  priority=self.priority, pin=self.pin)]
 
             for setting in usettings:
                 if not self.rawurl:
